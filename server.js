3cc584a5ceed3f3ecb67fafbf02d0991e697ef7d
AndraMeran <andrameran@yahoo.it>
2026-04-21 19:47:08 +0200

    first commit avvio server backend

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import connectDB from './config/db.js'
+
+dotenv.config()
+
+connectDB()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({ message: 'SexyTeller API is running' })
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`Server in ascolto sulla porta ${PORT}`)
+})
