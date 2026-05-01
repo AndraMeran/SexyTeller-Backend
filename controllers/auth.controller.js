9dfb9c87d8cba5857066fb80708da40eee6052f5
AndraMeran <andrameran@yahoo.it>
2026-05-01 12:43:44 +0200

    aggiunto name e handle nel token JWT

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -5,9 +5,9 @@ import { Resend } from 'resend'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
-const generateToken = (id, isRedazione) => {//si crea un token Jwt che ogni utente riceve al momento della reg o login
-    return jwt.sign(//contiene 
-        { id, isRedazione },
+const generateToken = (id, isRedazione, name, handle) => { //si crea un token Jwt che ogni utente riceve al momento della reg o login
+    return jwt.sign( //contiene 
+        { id, isRedazione, name, handle },
         process.env.JWT_SECRET,
         { expiresIn: '30d' }
     )
@@ -17,7 +17,7 @@ export async function register(req, res) {
     try {
         const { name, handle, email, password, birthDate } = req.body
 
-        const today = new Date()//funzione per controllare se maggiorene-  monthDiff serve per essere precisi —il semplice calcolo degli anni ti darebbe un anno in più. Se ha meno di 18 anni, blocca tutto e risponde con errore.
+        const today = new Date() //funzione per controllare se maggiorene- monthDiff serve per essere precisi —il semplice calcolo degli anni ti darebbe un anno in più. Se ha meno di 18 anni, blocca tutto e risponde con errore.
         const birth = new Date(birthDate)
         let age = today.getFullYear() - birth.getFullYear()
         const monthDiff = today.getMonth() - birth.getMonth()
@@ -29,19 +29,19 @@ export async function register(req, res) {
             return res.status(400).json({ message: 'Devi avere almeno 18 anni per registrarti' })
         }
 
-        const emailExists = await User.findOne({ email })//controllo se email esiste
+        const emailExists = await User.findOne({ email }) //controllo se email esiste
         if (emailExists) {
             return res.status(400).json({ message: 'Email già registrata' })
         }
 
-        const handleExists = await User.findOne({ handle })//controllo se handle esiste
+        const handleExists = await User.findOne({ handle }) //controllo se handle esiste
         if (handleExists) {
             return res.status(400).json({ message: 'Handle già in uso' })
         }
 
-        const hashedPassword = await bcrypt.hash(password, 10)//hash della pasword
+        const hashedPassword = await bcrypt.hash(password, 10) //hash della password
 
-        const user = await User.create({//qui creiamo l'utente nel database
+        const user = await User.create({ //qui creiamo l'utente nel database
             name,
             birthDate,
             handle,
@@ -49,7 +49,7 @@ export async function register(req, res) {
             password: hashedPassword
         })
 
-        await resend.emails.send({//l'invio della email di benvenuto
+        await resend.emails.send({ //l'invio della email di benvenuto
             from: 'onboarding@resend.dev',
             to: user.email,
             subject: 'Benvenuto su SexyTeller',
@@ -62,9 +62,9 @@ export async function register(req, res) {
             Il team di SexyTeller</p>`
         })
 
-        return res.status(201).json({//risposta al frontend con reg completata e generato token
+        return res.status(201).json({ //risposta al frontend con reg completata e generato token
             message: 'Registrazione completata',
-            token: generateToken(user._id, user.isRedazione),
+            token: generateToken(user._id, user.isRedazione, user.name, user.handle),
             isRedazione: user.isRedazione
         })
 
@@ -73,23 +73,23 @@ export async function register(req, res) {
     }
 }
 
-export async function login(req, res) {//ricerca l'utente per email
+export async function login(req, res) { //ricerca l'utente per email
     try {
         const { email, password } = req.body
 
         const user = await User.findOne({ email })
         if (!user) {
-            return res.status(401).json({ message: 'Credenziali errate' })//se non esiste credenziali errate
+            return res.status(401).json({ message: 'Credenziali errate' }) //se non esiste credenziali errate
         }
 
-        const result = await bcrypt.compare(password, user.password)//Confronta la password inserita con quella criptata nel database usando bcrypt.compare 
+        const result = await bcrypt.compare(password, user.password) //Confronta la password inserita con quella criptata nel database usando bcrypt.compare
         if (!result) {
-            return res.status(401).json({ message: 'Credenziali errate' })//se non coincidono 
+            return res.status(401).json({ message: 'Credenziali errate' }) //se non coincidono
         }
 
-        return res.status(200).json({//se tutto ok risponde con il token 
+        return res.status(200).json({ //se tutto ok risponde con il token
             message: 'Login effettuato',
-            token: generateToken(user._id, user.isRedazione),
+            token: generateToken(user._id, user.isRedazione, user.name, user.handle),
             isRedazione: user.isRedazione
         })
 
@@ -98,11 +98,11 @@ export async function login(req, res) {//ricerca l'utente per email
     }
 }
 
-export async function googleCallback(req, res) {//Questa funzione viene chiamata dopo che Google ha verificato l'identità dell'utente.
+export async function googleCallback(req, res) { //Questa funzione viene chiamata dopo che Google ha verificato l'identità dell'utente.
     try {
         const user = req.user
 
-        const token = generateToken(user._id, user.isRedazione)
+        const token = generateToken(user._id, user.isRedazione, user.name, user.handle)
 
         return res.redirect(
             `http://localhost:5173/login-success?token=${token}&isRedazione=${user.isRedazione}`
